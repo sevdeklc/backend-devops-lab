03123b6d9e9e0399fc9aba2d30c03ef673a530d9
Sevde Kılıç <sevdek332@gmail.com>
2026-06-27 14:38:56 +0300

    feat: initialize Spring Boot project with core dependencies

---
+++ task-manager-api/src/test/java/dev/backendlab/taskmanager/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.backendlab.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
