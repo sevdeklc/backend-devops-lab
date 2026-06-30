fee46ebe2613df8587aecc67de0977c50f4725e4
Sevde Kılıç <sevdek332@gmail.com>
2026-06-30 19:47:30 +0300

    test: add dedicated test profile with H2 database

--- task-manager-api/src/test/java/dev/backendlab/taskmanager/TaskManagerApiApplicationTests.java
+++ task-manager-api/src/test/java/dev/backendlab/taskmanager/TaskManagerApiApplicationTests.java
@@ -2,8 +2,10 @@ package dev.backendlab.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskManagerApiApplicationTests {
 
 	@Test
